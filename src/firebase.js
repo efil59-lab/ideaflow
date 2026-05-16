fb802a2f56ddf1ff4c9353413e2e4f0048ed68ef
efil59-lab <efil59@gmail.com>
2026-05-16 21:37:03 +0300

    login

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWJuVwuPaEG_BZkHBWovFQWqogW7bHtDY",
-  authDomain: "ideaflow-81aee.firebaseapp.com",
-  databaseURL: "https://ideaflow-81aee-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "ideaflow-81aee",
-  storageBucket: "ideaflow-81aee.firebasestorage.app",
-  messagingSenderId: "707034217510",
-  appId: "1:707034217510:web:004d5c6bf68a9f2669bae7",
-  measurementId: "G-CC3XLBBKJ0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
